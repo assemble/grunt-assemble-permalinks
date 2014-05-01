9e05e2f8b982620c11b31088edfb5a9b0458ee87
jonschlinkert <dev@sellside.com>
2014-05-01 04:13:23 -0400

    rename file to index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var path  = require('path');
 // node_modules
 var async = require('async');
 var permalinks = require('permalinks');
+var calculatePath = require('calculate-assets');
 var _str = require('underscore.string');
 var _ = require('lodash');
 var utils  = require('./lib/utils');
@@ -24,15 +25,15 @@ var utils  = require('./lib/utils');
  * @param  {Function} done
  * @return {String}   The permalink string
  */
-module.exports = function (assemble) {
 
+module.exports = function (assemble) {
 
   var plugin = function(params, done) {
 
     var grunt          = assemble.config.grunt;
     var options        = assemble.config.permalinks;
-    var pages          = assemble.pages;
     var originalAssets = assemble.config.assets;
+    var pages          = assemble.pages;
 
     // Skip over the plugin if it isn't defined in the options.
     if(!_.isUndefined(options)) {
@@ -42,7 +43,6 @@ module.exports = function (assemble) {
       options.length = pageKeys.length;
 
       async.forEach(pageKeys, function(pageKey, next) {
-
 
         var page = pages[pageKey];
         options.index++;
@@ -57,18 +57,12 @@ module.exports = function (assemble) {
         // Convenience variable for YAML front matter.
         var yfm  = page.data;
 
-        /**
-         * EXCLUSION PATTERNS OPTION
-         * Properties to omit from the params object.
-         */
+        // exclusion patterns option. properties to omit from the params object.
         var exclusions = ['_page', 'data', 'filePair', 'page', 'pageName'];
             exclusions = _.union([], exclusions, options.exclusions || []);
 
 
-        /**
-         * `slugify` option
-         * Ensure that basenames are suitable to be used as URLs.
-         */
+        // `slugify` option. Ensure that basenames are suitable to be used as URLs.
         if(options.slugify) {
           if(!yfm.slug) {
             page.data.slug = _str.slugify(page.data.basename);
@@ -82,53 +76,43 @@ module.exports = function (assemble) {
          * @examples
          *   010foo.html,011bar.html => foo.html,bar.html
          */
+
         if(options.stripnumber === true) {
           page.data.basename = page.data.basename.replace(/^\d+\-?/, '');
         }
 
 
-
-
         var context = _.extend({}, yfm, page);
         context = _.omit(context, exclusions);
-
-
-        // Generate a javascript file with all non-function replacement patterns
-        if(options.debug) {
-          if(typeof(options.debug) === 'string') {
-            //grunt.file.write(options.debug, require('util').inspect(replacements));
-          } else {
-            throw console.error('"options.debug" must be a file path.');
-          }
-        }
 
         /**
          * CREATE PERMALINKS
          * Construct the permalink string. Modifies string with an array
          * of replacement patterns passed into options.patterns
          */
+
         var permalink = permalinks(structure, context, options);
+
 
         /**
          * WRITE PERMALINKS
          * Append the permalink to the dest path defined in the target.
          */
+
         if(_.isUndefined(options.structure) && _.isEmpty(permalink)) {
           page.data.dest = page.dest = page.data.dest || page.dest;
         } else {
           if (page.data.basename === 'index') {
             page.data.dest = page.dest = page.data.dest || page.dest;
           } else {
-            page.data.dest = page.dest = utils.normalizePath(path.join(page.data.dirname, permalink));
+            page.data.dest = page.dest = path.join(page.data.dirname, permalink).replace(/\\/, '/');
           }
         }
-
-        page.data.assets = utils.calculatePath(page.data.dest, originalAssets, originalAssets);
+        page.data.assets = calculatePath(page.data.dest, originalAssets);
 
         grunt.verbose.ok('page'.yellow, page);
         grunt.verbose.ok('page.data.dest'.yellow, page.data.dest);
         grunt.verbose.ok('page.data.assets'.yellow, page.data.assets);
-        grunt.verbose.ok('Generated permalink for:'.yellow, page.data.dest);
         next();
       });
 
@@ -148,6 +132,5 @@ module.exports = function (assemble) {
   var rtn = {};
   rtn[plugin.options.name] = plugin;
   return rtn;
-
 };
 
